d7c066cd12e4a6b37103e25227133d72fdfcb87c
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-06-18 21:23:03 +0200

    add logged/not logged logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import { Root } from './routes';
+import React, { useEffect, useState } from 'react';
+import { Root, Auth } from './routes';
 import { TodoProvider } from './contexts';
+import auth from '@react-native-firebase/auth';
 
 export const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return () => subscriber(); // unsubscribe on unmount
+  }, []);
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+  }
+
+  if (!user) {
+    return <Auth />;
+  }
+
   return (
     <TodoProvider>
       <Root />
