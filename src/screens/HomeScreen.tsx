89e3dce548fa464b18c37b1f8be6eb4ed36e649c
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-06-21 22:38:24 +0200

    update home screen styles

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,78 @@
 import React, { FC } from 'react';
-import { SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import { Button } from '../components';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../Interfaces';
+import { useTodos } from '../hooks';
+import { ScrollView } from 'react-native-gesture-handler';
 
 interface Props {
   navigation: StackNavigationProp<RootStackParamList, 'Home'>;
 }
 
+const Separator = () => <View style={styles.separator} />;
+const Line = () => <View style={styles.line} />;
+
 export const HomeScreen: FC<Props> = ({ navigation }) => {
+  const { todos } = useTodos();
   return (
-    <SafeAreaView>
-      <Button title="todos" onPress={() => navigation.navigate('Todos')} />
-    </SafeAreaView>
+    <ScrollView>
+      <View style={styles.tile}>
+        <Text style={styles.heading}>Check what needs to be done</Text>
+        <Separator />
+        <Line />
+        <Separator />
+        <Text style={styles.todosHeading}>Latest Todo</Text>
+        <Text>
+          {todos[0].name} added by {todos[0].user}
+        </Text>
+        <Separator />
+        <Line />
+        <Separator />
+        <Button
+          type="primary"
+          title="Todos"
+          onPress={() => navigation.navigate('Todos')}
+        />
+      </View>
+    </ScrollView>
   );
 };
+
+const margin = 16;
+const width = Dimensions.get('window').width - margin * 2;
+
+const styles = StyleSheet.create({
+  todosHeading: {
+    fontWeight: '600',
+  },
+  separator: {
+    marginBottom: 12,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  line: {
+    width: '100%',
+    height: 2,
+    backgroundColor: '#eee',
+    borderRadius: 2,
+  },
+  tile: {
+    width,
+    backgroundColor: 'white',
+    margin,
+    padding: margin,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.08,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+});
