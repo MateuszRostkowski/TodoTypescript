5df70e3cd67d2f496e75a9f581d17affe6fb7487
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-06-22 22:29:16 +0200

    add styled system box

--- src/components/Box.tsx
+++
@@ -1,6 +0,0 @@
-import React, { FC } from 'react';
-import { View } from 'react-native';
-
-export const Box: FC = ({ children, ...rest }) => {
-  return <View {...rest}>{children}</View>;
-};
---
+++ src/components/_atoms.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+import { FC } from 'react';
+
+import {
+  space,
+  SpaceProps,
+  flexbox,
+  FlexProps,
+  border,
+  BorderProps,
+  color,
+  ColorProps,
+  position,
+  PositionProps,
+  layout,
+  LayoutProps,
+  size,
+  SizeProps,
+} from 'styled-system';
+type BoxProps = FC &
+  ColorProps &
+  SpaceProps &
+  FlexProps &
+  BorderProps &
+  PositionProps &
+  LayoutProps &
+  SizeProps;
+
+const common = [space, flexbox, border, color, position, layout, size];
+
+export const Box = styled.Text<BoxProps>`
+  ${common}
+`;
