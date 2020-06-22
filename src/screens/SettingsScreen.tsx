c4aff9ac9b5d2a1bac1e2ccb23dd5a9ba6d72e49
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-06-22 22:29:51 +0200

    add change name to settings screen

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,4 +1,39 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, View, StyleSheet, Text } from 'react-native';
+import { Button, Input, Box } from '../components';
+import { updateUserName, getCurrentUser } from '../services';
 
-export const SettingsScreen = () => <Text>Settings</Text>;
+export const SettingsScreen = () => {
+  const [name, setName] = useState('');
+  const [loading, setLoading] = useState(false);
+  const user = getCurrentUser();
+
+  const submit = async () => {
+    setLoading(true);
+    await updateUserName(name);
+    setLoading(false);
+  };
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <Text>Hello {user} you can change your name if you want</Text>
+      <Box mt="4px" />
+      <Input placeholder="name" value={name} onChangeText={setName} />
+      <Box mt="4px" />
+      <Button
+        loading={loading}
+        type="primary"
+        title="Change name"
+        onPress={submit}
+      />
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
