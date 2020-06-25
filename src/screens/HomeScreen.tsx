59045fbe78d73831b17c89149dfdfebdb2b8ae73
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-06-25 18:09:32 +0200

    clean up homescreen; add styled components to homescreen; add CreateTodosListTile without logic;

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
-import { Button } from '../components';
+import React, { FC, useState } from 'react';
+import { Text, StyleSheet, Dimensions, TextInputProps } from 'react-native';
+import styled from 'styled-components/native';
+import { Button, Input, Box } from '../components';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../Interfaces';
 import { useTodos } from '../hooks';
@@ -11,48 +12,97 @@ interface Props {
   navigation: StackNavigationProp<RootStackParamList, 'Home'>;
 }
 
-const Separator = () => <View style={styles.separator} />;
-const Line = () => <View style={styles.line} />;
+const margin = 16;
+const width = Dimensions.get('window').width - margin * 2;
+
+const Tile = styled.View`
+  width: ${width}px;
+  background-color: white;
+  margin: ${margin / 2}px ${margin}px;
+  padding: ${margin}px;
+  border-radius: 8px;
+  shadow-color: #000;
+  shadow-offset: {
+    width: 0;
+    height: 2;
+  }
+  shadow-opacity: 0.08;
+  shadow-radius: 3.84px;
+  elevation: 5;
+`;
+
+const Line = styled.View`
+  width: 100%;
+  height: 2px;
+  background-color: #eee;
+  border-radius: 2px;
+`;
+
+const Separator = () => <Box mt="4px" />;
+
+const SeparatorWithLine = () => (
+  <>
+    <Separator />
+    <Line />
+    <Separator />
+  </>
+);
+
+const StyledInput = styled(Input)`
+  background-color: #f1f1f1;
+  margin: 0;
+  width: 100%;
+`;
+
+const CreateTodosListTile = ({ navigation }) => {
+  const [name, setName] = useState('');
+  return (
+    <Tile>
+      <Text style={styles.heading}>Create new list</Text>
+      <SeparatorWithLine />
+      <StyledInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Type new list name"
+      />
+      <SeparatorWithLine />
+      <Button type="primary" title="Create" onPress={() => alert(name)} />
+    </Tile>
+  );
+};
 
 export const HomeScreen: FC<Props> = ({ navigation }) => {
   const { todos } = useTodos();
   const user = getCurrentUser();
+
   return (
     <ScrollView>
       <Separator />
-      <View style={styles.tile}>
+      <Tile>
         <Text style={styles.heading}>Hello {user}</Text>
-        <Separator />
-        <Line />
-        <Separator />
+        <SeparatorWithLine />
         <Text style={styles.todosHeading}>
           Check out todos what you need to finish
         </Text>
-      </View>
-      <View style={styles.tile}>
+      </Tile>
+      <CreateTodosListTile navigation={navigation} />
+      <Tile>
         <Text style={styles.heading}>Check what needs to be done</Text>
-        <Separator />
-        <Line />
-        <Separator />
+        <SeparatorWithLine />
         <Text style={styles.todosHeading}>Latest Todo</Text>
         <Text>
           {todos[0]?.name} added by {todos[0]?.user}
         </Text>
-        <Separator />
-        <Line />
-        <Separator />
+        <SeparatorWithLine />
         <Button
           type="primary"
-          title="Todos"
+          title="Navigate"
           onPress={() => navigation.navigate('Todos')}
         />
-      </View>
+      </Tile>
     </ScrollView>
   );
 };
-
-const margin = 16;
-const width = Dimensions.get('window').width - margin * 2;
 
 const styles = StyleSheet.create({
   todosHeading: {
