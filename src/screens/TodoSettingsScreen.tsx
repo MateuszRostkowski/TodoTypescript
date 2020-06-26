c5cf6a6a930511b65d0824dd25464ad66c075949
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-06-26 13:10:43 +0200

    add todo settings screen

---
+++ src/screens/TodoSettingsScreen.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { ScrollView, Text } from 'react-native';
+
+export const TodoSettingsScreen: FC = () => {
+  return (
+    <ScrollView>
+      <Text>todoSettings</Text>
+    </ScrollView>
+  );
+};
