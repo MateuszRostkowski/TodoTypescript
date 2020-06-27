a0f7db1863d90e8a3919961b45a440f3718ecd4a
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-06-27 23:06:05 +0200

    add useCurrentTodoList hook

--- src/hooks/useTodoLists.tsx
+++ src/hooks/useTodoLists.tsx
@@ -2,3 +2,8 @@ import { useContext } from 'react';
 import { TodoListsContext } from '../contexts';
 
 export const useTodoLists = () => useContext(TodoListsContext);
+
+export const useCurrentTodoList = (id: string) => {
+  const { userTodoLists } = useTodoLists();
+  return userTodoLists.find((todoList) => todoList.id === id);
+};
