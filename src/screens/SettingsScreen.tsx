bbf86c0d10ac8ba4275c7579ef595815214981dc
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-07-12 21:45:50 +0200

    add current user email in settings

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text } from 'react-native';
-import { Button, Input, Box, KeyboardAwareScrollView } from '../components';
+import { StyleSheet } from 'react-native';
+
+import {
+  Button,
+  Input,
+  Box,
+  KeyboardAwareScrollView,
+  Text,
+  ListItem,
+} from '../components';
 import { logout } from '../services';
 import { useAuth } from '../hooks';
 
@@ -16,9 +24,13 @@ export const SettingsScreen = () => {
   };
   return (
     <KeyboardAwareScrollView contentContainerStyle={styles.container}>
-      <Text>
-        Hello {user?.displayName} you can change your name if you want
-      </Text>
+      <Box width={'100%'}>
+        <ListItem title={`User name: ${user?.displayName}`} />
+        <ListItem title={`Email: ${user?.email}`} />
+      </Box>
+
+      <Box mt="10px" />
+      <Text>You can change your name if you want</Text>
       <Box mt="4px" />
       <Input placeholder="name" value={name} onChangeText={setName} />
       <Box mt="4px" />
